8c997483898e55983494e0fa217104c5d2444a0b
dave <dave@blc-services.co.uk>
2015-03-19 08:54:33 +0000

    Inital project framework created.

---
+++ smartgenie-crm/src/main/java/uk/co/blc_services/smartgenie_crm/App.java
@@ -0,0 +1,13 @@
+package uk.co.blc_services.smartgenie_crm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
